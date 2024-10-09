620ea408e510c9e4e9c11ed72576f0df439c87c6
dilip <dilip@LAPTOP-C1GOMV40>
2024-10-09 19:20:02 +0530

    This  is the print and variables

---
+++ JavaPrograms/src/ProgramPack1/Print.java
@@ -0,0 +1,17 @@
+package ProgramPack1;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// This program helps to learn how to print the message
+		System.out.println("Hii..!");
+		System.out.println("This is sowmiya");
+		System.out.println("studing college second year");
+		System.out.println("This project we can learn the topics from core java");
+		System.out.println("Java programs");
+		System.out.println("This system.out.println helps to print the messages");
+		System.out.println("Thank You!");
+	}
+
+}
---
+++ JavaPrograms/src/ProgramPack1/Variables.java
@@ -0,0 +1,22 @@
+package ProgramPack1;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// how to declare variables and assigning values to variable
+		byte by = 89;
+		short sh = 8905;
+		int in = 90909090;
+		long ln = 1234567890;
+		double du = 2345.890;
+		float fl = 234.56789f;
+		System.out.println(by);
+		System.out.println(sh);
+		System.out.println(in);
+		System.out.println(ln);
+		System.out.println(du);
+		System.out.println(fl);
+	}
+
+}
